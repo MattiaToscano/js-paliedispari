9c20374cb0ad18613dd0399137c4826656723ad7
MattiaToscano <mattia1997ct@gmail.com>
2025-03-21 13:31:47 +0100

    [UPD] Finalizzata funzione e esercizio completato

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ let parola = prompt('Inserisci una parola');
 
 //Creo funzione per capire se la parola è palindroma o no.
 function palindroma(parola){
-
-
+    let parolaInversa = parola.split('').reverse().join('');
+    if(parola == parolaInversa){
+        return 'La parola è palindroma';
+    }else{
+        return 'La parola non è palindroma';
+    }
 }
+
+    console.log(palindroma(parola));
 
 
 
