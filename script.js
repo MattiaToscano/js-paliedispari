9f2761c8a30eb820e4ed1cb30dedb49995dcebfc
MattiaToscano <mattia1997ct@gmail.com>
2025-03-21 13:36:58 +0100

    [UPD] Inizializzata variabile utente e funzione PC

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 console.log('Esercizio 1');
 
-//Chiediamo al cliente di inserire una parola
+//Chiediamo al cliente di inserire una parola.
 let parola = prompt('Inserisci una parola');
 
 //Creo funzione per capire se la parola è palindroma o no.
@@ -13,8 +13,24 @@ function palindroma(parola){
         return 'La parola non è palindroma';
     }
 }
-
+    //Stampo il risultato.
     console.log(palindroma(parola));
+
+console.log('Esercizio 2 ');
+
+//Chiediamo all'utente di inserire un numero pari o dispari e successivamente un numero da 1 a 5.
+let pariDispari = prompt('Scegli tra pari o dispari');
+let numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
+
+function numeroRandom(min, max){
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+    let max = 5;
+    let min = 1;
+    
+}
+
+
 
 
 
