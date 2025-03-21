004e6dbc729ba43d4acb738a321fe3d0d1d06be9
MattiaToscano <mattia1997ct@gmail.com>
2025-03-21 13:26:05 +0100

    [UPD] Inizializzata funzione e variabile

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log('Esercizio 1');
+
+//Chiediamo al cliente di inserire una parola
+let parola = prompt('Inserisci una parola');
+
+//Creo funzione per capire se la parola è palindroma o no.
+function palindroma(parola){
+
+
+}
+
+
+
+
